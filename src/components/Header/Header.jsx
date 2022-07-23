37fb9ffeda70eb26a0fd746a00244f21380db41b
zakhep66 <sur.mikhael@gmail.com>
2022-07-24 02:02:40 +0300

    dynamic items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,17 @@ import Logo from "./Logo";
 import Lang from "./Lang";
 import Menu from "./Menu";
 import { useState } from "react";
+import {observer} from "mobx-react";
+import HeaderStore from "../../store/HeaderStore";
 
-const Header = () => {
-  const items = [{value: "О станции", href:"#s"}, {value: "Наша команда", href:"#s"}, {value: "Как это работает", href:"#s"}, {value: "Новости", href:"#s"}, {value: "Контакты", href:"#s"}]
+const Header = observer(() => {
   const [modalActive, setModalActive] = useState(false)
   return (
     <div className="container">
       <header className="header">
         <div className="header__adaptive">
           <Logo />
-          <Menu items={items} active={modalActive} setActive={setModalActive}/>
+          <Menu items={HeaderStore.items} active={modalActive} setActive={setModalActive}/>
           <div className="header__rigth-block">
             <Lang />
             <button onClick={() => setModalActive(!modalActive)} className={modalActive ? "burg__menu burger--active" : "burg__menu"}>
@@ -22,6 +23,6 @@ const Header = () => {
       </header>
     </div>
   );
-};
+});
 
 export default Header;
