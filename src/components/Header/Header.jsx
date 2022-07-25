91263c1ee3757544cb9a000e497f5fdeed19e87c
OsipovArsen <arsenij-osipov@mail.ru>
2022-07-25 15:12:55 +0300

    header+

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,26 @@ import Logo from "./Logo";
 import Lang from "./Lang";
 import Menu from "./Menu";
 import { useState } from "react";
-import {observer} from "mobx-react";
+import { observer } from "mobx-react";
 import HeaderStore from "../../store/HeaderStore";
 
 const Header = observer(() => {
-  const [modalActive, setModalActive] = useState(false)
+  const [modalActive, setModalActive] = useState(false);
   return (
     <div className="container">
+      <Menu items={items} active={modalActive} setActive={setModalActive} />
       <header className="header">
         <div className="header__adaptive">
           <Logo />
-          <Menu items={HeaderStore.items} active={modalActive} setActive={setModalActive}/>
           <div className="header__rigth-block">
             <Lang />
-            <button onClick={() => setModalActive(!modalActive)} className={modalActive ? "burg__menu burger--active" : "burg__menu"}>
-              <span/>
+            <button
+              onClick={() => setModalActive(!modalActive)}
+              className={
+                modalActive ? "burg__menu burger--active" : "burg__menu"
+              }
+            >
+              <span />
             </button>
           </div>
         </div>
