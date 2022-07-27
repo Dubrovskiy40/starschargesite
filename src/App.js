670515d81b7714c2a3df2fa92e4af84cf14b9477
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-07-27 23:59:08 +0300

    стили для input tel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/";
-import {Cards, Feedback} from "./components";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Feedback />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
