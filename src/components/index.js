10cb1a6fc837c91854244e7f5d1ba08bb3e66e7c
Vsevolod <slezhov@yandex.ru>
2022-07-31 01:04:58 +0300

    footer+scrollbtn

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Cards } from "./Cards/Cards";
 export { default as Header } from "./Header/Header";
 export { default as Feedback } from "./Feedback/Feedback";
-export { default as Foter } from "./Foter/Foter";
+export { default as Foooter } from "./Footer/Footer";
 export { default as ScrollButton } from "./ScrollButton/ScrollButton";
