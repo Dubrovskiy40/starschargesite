12952e8bb0c570b59448d22b2691bdf1a4dd9cc9
Vsevolod <slezhov@yandex.ru>
2022-07-31 00:59:18 +0300

    footer+scrollbtn

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Cards, Header, Feedback, Footer, ScrollButton } from "../../components";
+import { Cards, Header, Feedback, Foter, ScrollButton } from "../../components";
 import MapContainer from "../../components/MapContainer/MapContainer";
 
 function Home() {
@@ -8,7 +8,7 @@ function Home() {
       <Cards />
       {/* <Feedback /> */}
       <MapContainer />
-      <Footer />
+      <Foter />
       <ScrollButton />
     </>
   );
