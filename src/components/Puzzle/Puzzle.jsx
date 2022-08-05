4f17d4a44cd29c70113c9448ec143b60450654c7
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-08-05 13:18:50 +0600

    adaptive puzzle

--- src/components/Puzzle/Puzzle.jsx
+++ src/components/Puzzle/Puzzle.jsx
@@ -9,7 +9,7 @@ const TextOfCard = {
     title:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
     paragraph:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente ng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eesse cupi",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquceat, magni aliquid?piente ng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eesse cupiceat, magni aliquid?piente ng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eesse cupiid?piente ng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eng elit. Enim consequuntur beatae quibusdam quis ad repellat, eaque sapiente esse cupiditate, deserunt hic expedita corrupti porro repudiandae, eligendi amet. Placeat, magni aliquid?piente eesse cupi",
     dataPublic: "18.07.2022",
 }
 
@@ -21,7 +21,7 @@ const getCards = (currentCard, visibleCards) => {
 
     for (let index = currentCard; index < currentCard + visibleCards; index++) {
         content.push(<div
-            className={(index === currentCard ? Style.grid__firstCell : ((index === currentCard + 1) & (visibleCards === 4)) ? Style.grid__secondCell : Style.grid__restCell)} > <CardForPuzzle id={`c${index}`} imgPath=
+            className={(index === currentCard ? Style.grid__firstCell : ((index === currentCard + 1) & (visibleCards === 4 || visibleCards === 3)) ? Style.grid__secondCell : Style.grid__restCell)} > <CardForPuzzle id={`c${index}`} imgPath=
                 {TextOfCard.imgPath}
                 title={`${index} ${TextOfCard.title}`}
                 paragraph={TextOfCard.paragraph}
@@ -44,7 +44,8 @@ export const useWindowSize = () => {
         return () => window.removeEventListener('resize', handleResize)
     }, [size])
 
-    return (size[0] < 480 ? 1 : (size[0] < 768 ? 3 : (size[0] < 992 ? 3 : (size[0] < 1200 ? 5 : 4))))
+    // return (size[0] < 480 ? 3 : (size[0] < 768 ? 3 : (size[0] < 1200 ? 5 : 4)))
+    return (size[0] < 768 ? 3 : (size[0] < 1200 ? 5 : (size[0] < 2000 ? 4 : 8)))
 }
 
 const Puzzle = () => {
