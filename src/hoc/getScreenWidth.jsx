4323b43abba6d33936faec3586df14669e9665aa
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-08-07 10:13:21 +0300

    add hoc

---
+++ src/hoc/getScreenWidth.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+
+export function getScreenWidth(Component) {
+  let WrapperContainer = (props) => {
+    const [widthScreen, setWidthScreen] = useState(window.innerWidth);
+    const [deviceType, setDeviceType] = useState("desctop"); //desctop, tablet, mobile
+
+    const updateDimensions = () => {
+      setWidthScreen(window.innerWidth);
+    };
+
+    useEffect(() => {
+      window.addEventListener("resize", updateDimensions);
+
+      if (widthScreen > 1190) {
+        setDeviceType("desctop");
+      }
+      if (widthScreen > 768 && widthScreen <= 1190) {
+        setDeviceType("tablet");
+      }
+      if (widthScreen <= 768) {
+        setDeviceType("mobile");
+      }
+
+      return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+
+    return (
+      <Component {...props} deviceType={deviceType} widthScreen={widthScreen} />
+    )
+  }
+
+  return WrapperContainer;
+};
