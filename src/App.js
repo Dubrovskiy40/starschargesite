7d3c2955c9b18f25be83bd0f1229ae4258d34d57
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-08-09 18:38:35 +0300

    add ru/en

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/";
 import MapContainer from "./components/MapContainer/MapContainer";
-import Loader from "./components/Loader/Loader";
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
