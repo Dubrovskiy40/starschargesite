9b18a83f2c9ede5c74df84c3f16e6a7ed50f2b1c
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-08-11 18:56:52 +0600

    add Link to card

--- src/components/Puzzle/CardForPuzzle/CardForPuzzle.jsx
+++ src/components/Puzzle/CardForPuzzle/CardForPuzzle.jsx
@@ -1,23 +1,26 @@
 import Style from "./cards.module.scss"
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom"
 
 const CardForPuzzle = ({ id, imgPath, title, paragraph, dataPublic }) => {
 
     return (
-        <li key={id} className={Style.card} >
-            <img className={Style.card__img} src={imgPath} alt="img" />
-            <div className={Style.card__text_block}>
-                <h3 className={Style.card__title}>
-                    {title}
-                </h3>
-                <p className={Style.card__paragraph}>
-                    {paragraph}
-                </p> 
-                <span className={Style.card__data}>
-                    {dataPublic}
-                </span>
-            </div>
-        </li>
+        <Link to="idCard" className={Style.card__link}>
+            <li key={id} className={Style.card} >
+                <img className={Style.card__img} src={imgPath} alt="img" />
+                <div className={Style.card__text_block}>
+                    <h3 className={Style.card__title}>
+                        {title}
+                    </h3>
+                    <p className={Style.card__paragraph}>
+                        {paragraph}
+                    </p> 
+                    <span className={Style.card__data}>
+                        {dataPublic}
+                    </span>
+                </div>
+            </li>
+        </Link>
     );
 };
 
