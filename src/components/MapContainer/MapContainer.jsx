4c24b30fb319f1efad72646674052dcdc6f8f23d
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-08-15 21:49:35 +0300

    update develop branch

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -8,7 +8,7 @@ const MapContainer = (props) => {
       <div className="container">
         <div className="map__main">
           <h2 className="map__title">{t("mapContainer.title")}</h2>
-          <section className="map__yandex">
+          <section className="map__yandex ">
             <div className="map-frame">
               <iframe
                 src="https://yandex.ru/map-widget/v1/?um=constructor%3A5919a4499abe294201804f138c5c39eb9519b5e6b5fd791b20adf349051d6dc3&amp;source=constructor"
