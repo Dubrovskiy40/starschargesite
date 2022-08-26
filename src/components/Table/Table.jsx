7b4eb97e2f87fe1aa779b91fe2d6e8c045bbb4ba
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-08-26 12:22:13 +0600

    add props at Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,71 +1,21 @@
 import Style from "./table.module.scss";
 
-const Table = () => {
-  const textOfCardTable = [
-    {
-      id: "1",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-    {
-      id: "2",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-    {
-      id: "3",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-    {
-      id: "4",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-    {
-      id: "5",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-    {
-      id: "6",
-      imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
-      title:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-      paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
-    },
-  ];
+const Table = ({cards}) => {
 
   return (
     <section className="table">
       <div className="container">
         <ul className={Style.tableGrid}>
-          {textOfCardTable?.map((content) => {
+          {cards.map((card) => {
             return (
-              <a className={Style.cell} key={content.id} href="https://www.asus.com/ru/" target="_blank" rel="noreferrer">
+              <a className={Style.cell} key={card.id} href="https://www.asus.com/ru/" target="_blank" rel="noreferrer">
                 <img
                   className={Style.img}
-                  src={content.imgPath}
+                  src={card.imgPath}
                   alt="product img"
                 ></img>
-                <h3 className={Style.title}>{content.title}</h3>
-                <p className={Style.paragraph}>{content.paragraph}</p>
+                <h3 className={Style.title}>{card.title}</h3>
+                <p className={Style.paragraph}>{card.paragraph}</p>
                 <button className={Style.button}>Подробнее</button>
               </a>
             );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,6 +16,8 @@ import Table from "../../components/Table/Table"
 function Home() {
   const [itemslocal, setItemsLocal] = useState([]);
 
+  const [ads, setAds] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       await MenuStore.fetchMenuItems().then(
@@ -25,8 +27,60 @@ function Home() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setAds([
+      {
+        id: "1",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+      {
+        id: "2",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+      {
+        id: "3",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+      {
+        id: "4",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+      {
+        id: "5",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+      {
+        id: "6",
+        imgPath: "https://envybox.io/blog/wp-content/uploads/2020/11/01.jpg",
+        title:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+        paragraph:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sint re.",
+      },
+    ]);
+  }, []);
   //const itemslocal = ;
-
+  
   return (
     <>
       <Header />
@@ -34,7 +88,7 @@ function Home() {
       <SwiperParallax menuName={itemslocal[1]} />
       <Cards menuName={itemslocal[2]} />
       <Team />
-      <Table />
+      <Table cards={ads}/>
       <MapContainer menuName={itemslocal[3]} />
       <Feedback />
       <ScrollButton />
