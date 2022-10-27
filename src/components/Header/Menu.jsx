abf20b2599210802d886c2c2b3de696d2b484e8d
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-10-27 23:49:12 +0300

    добавил перевод меню в хедер

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,18 +1,23 @@
 import { observer } from "mobx-react";
+import { useTranslation } from "react-i18next";
+import "../../utils/i18next";
 
 const Menu = observer(({ items, active, setActive }) => {
+  const { i18n } = useTranslation();
+  let lang = i18n.language;
+  
   return (
     <nav className="header__nav" onClick={() => setActive(false)}>
       <ul className="header__nav-list" onClick={(e) => e.stopPropagation()}>
         {items
-          .map((item, index) => (
+          .map((item) => (
             <li
-              key={index}
+              key={item.id}
               className="header__nav-item"
               onClick={() => setActive(false)}
             >
-              <a href={`#${item.name}`} className="header__nav-link">
-                {item.name}
+              <a href={`#${item.name_eng}`} className="header__nav-link">
+                {lang === 'ru' ? item.name : item.name_eng}
               </a>
             </li>
           ))}
