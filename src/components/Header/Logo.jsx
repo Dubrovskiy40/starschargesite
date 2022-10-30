3249c6ee2a567962fed1e8b5b3f17fd4910a5f72
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-10-31 01:47:42 +0700

    fix logo quality, fix cards in addDescr

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,15 +1,13 @@
 // import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo/logo.svg";
-import logo_tabl from "../../assets/images/logo/logo-1024.svg";
-import logo_desc from "../../assets/images/logo/logo-1500.svg";
+import logo_mobile from "../../assets/images/logo/logo-1500.svg";
 import "./Logo.scss";
 
 const Logo = () => {
   return (
     <a href="#Главная" className="header__logo-link">
       <picture>
-        <source srcSet={logo_tabl} media="(max-width: 1024px)" />
-        <source srcSet={logo_desc} media="(max-width: 1500px)" />
+        <source srcSet={logo_mobile} media="(max-width: 1500px)" />
         <img src={logo} alt="logo" className="header__logo"></img>
       </picture>
     </a>
